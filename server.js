61ee52ed778c0774219bd1d3410810ead34239f0
Chengula Jr <tungamalenganyumban@gmail.com>
2024-02-01 19:33:11 +0300

    initilizing the project

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: true }); // Fix typo here
+
+var mysql = require('mysql');
+
+var server = app.listen(8080, function () {
+    console.log('Server is listening on port 8080');
+});
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.set("view engine", "pug");
+
+app.get('/', function (req, res) { // Fix parentheses to curly braces
+    res.render('index');
+});
+
+app.get('/index', function (req, res) { // Fix parentheses to curly braces
+    res.render('index');
+});
+
+app.get('/contact', function (req, res) { // Fix parentheses to curly braces
+    res.render('contact');
+});
+app.get('/layout', function (req, res) { // Fix parentheses to curly braces
+    res.render('layout');
+});
+
+app.get('/login', function (req, res) { // Fix parentheses to curly braces
+    res.render('login');
+});
+
+app.get('/about', function (req, res) { // Fix parentheses to curly braces
+    res.render('about');
+});
